5fffd942c2e445d1787acc26ae3745029288876d
Chrisdale Allen Lau Candelario <chrisallen1394@gmail.com>
2020-06-23 02:32:14 +0800

    Updates

---
+++ precache-manifest.a0ecaddb925d277d6310fbb7888dc07d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3220eb6423d4184d1d7135b06402f235",
+    "url": "/Gist-API/index.html"
+  },
+  {
+    "revision": "f3e054507f3e30dd7386",
+    "url": "/Gist-API/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "a4e1fb77de6d98418361",
+    "url": "/Gist-API/static/css/main.930b67b1.chunk.css"
+  },
+  {
+    "revision": "f3e054507f3e30dd7386",
+    "url": "/Gist-API/static/js/2.7429d4af.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/Gist-API/static/js/2.7429d4af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4e1fb77de6d98418361",
+    "url": "/Gist-API/static/js/main.658ffdb5.chunk.js"
+  },
+  {
+    "revision": "8e87ff0fc4f076fe4b79",
+    "url": "/Gist-API/static/js/runtime-main.9c619cb1.js"
+  }
+]);
